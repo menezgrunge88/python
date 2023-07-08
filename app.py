0672e87acc7bb1786424789ff2a1afdaac32cba8
Sebastian <menezgrunge88@gmail.com>
2023-07-07 23:42:31 -0500

    Tercer commit

--- app.py
+++ app.py
@@ -7,7 +7,5 @@ print(now)
 #now = now + relativedelta(month=10, day=12, hour=7)
 print(now)
 
-
-
 print(relativedelta(datetime(2023, 7, 8, 2, 0, 0), now))
 print(relativedelta(now, datetime(2023, 7, 8, 2, 0, 0)))
--- backup.py
+++ backup.py
@@ -1,5 +1,5 @@
 import sys
 
-print(sys.argv)
-print(sys.argv[0])
-print(sys.argv[1])
+print(sys.argv) # Lista que contiene el nombre del programa en el índice cero y los argumentos en los demás indices
+print(sys.argv[0]) # Nombre del programa
+print(sys.argv[1]) # Primer argumento al llamar al programa.
